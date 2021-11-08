611da09a187af78f53447841c7ebed94deb6f501
Kevin <okkevin96@hotmail.it>
2021-11-08 16:14:49 +0100

    funziona tutto, ma meglio ridarci un occhio

--- GROUP_06.cydsn/InterruptRoutines.c
+++ GROUP_06.cydsn/InterruptRoutines.c
@@ -14,24 +14,25 @@
 #define NUMBER_OF_SAMPLES 5
 #define CONTROL_REG0 0
 #define CONTROL_REG1 1
+#define STATO0 0b00010100   //in exadecimale 14 SONO I COMANDI DA INSERIRE NEL BRIDGE CONTROL
+#define STATO1 0b00010101   //in exadecimale 15
+#define STATO2 0b00010110   //in exadecimale 16
+#define STATO3 0b00010111   //in exadecimale 17
 
 
 // Include header
 #include "InterruptRoutines.h"
 // Include required header files
 #include "project.h"
+
 // Variables declaration */
-int32 value_digit;
+int32 value_digit; //support variable for ADC sampling
 int32 value_temp;
 int32 value_ph;
 int32 Temp_mean;
 int32 Ph_mean;
-int32 counter_SP=0; //counter dei campioni
+int32 counter_SP=0; // sampling counter
 
-#define STATO0 0b00010100   //in exadecimale 14 SONO I COMANDI DA INSERIRE NEL BRIDGE CONTROL
-#define STATO1 0b00010101   //in exadecimale 15
-#define STATO2 0b00010110   //in exadecimale 16
-#define STATO3 0b00010111   //in exadecimale 17
 
 
 // Our global variables
@@ -75,7 +76,8 @@ CY_ISR(Custom_ISR_ADC)
     
     //select the Temp channel  
 
-    if (status==1 || status==3 ){
+    if (status==1 || status==3 )
+    {
         AMux_FastSelect(CH_TEMP) ;
         // Read Timer status register to bring interrupt line low
         value_digit = ADC_DelSig_Read32();
@@ -84,31 +86,33 @@ CY_ISR(Custom_ISR_ADC)
         if (value_digit > 65535) 
             value_digit = 65535;
         value_temp = ADC_DelSig_CountsTo_mVolts(value_digit);// the sensibility is 10 mV\c
-        Temp_mean=Temp_mean+value_temp;}
+        Temp_mean=Temp_mean+value_temp;
+    }
 
 
-    if (status==2 || status==3 ){
+    if (status==2 || status==3 )
+    {
         //select the Photodiode channel
         AMux_FastSelect(CH_PH) ;
         // Read Timer status register to bring interrupt line low
         value_digit = ADC_DelSig_Read32();
         if (value_digit < 0) 
             value_digit = 0;
-        if (value_digit > 65535) 
+        if (value_digit > 65535)    
             value_digit = 65535;   
-        value_ph= ADC_DelSig_CountsTo_mVolts(value_digit);//da trovare il datasheet
+        value_ph= ADC_DelSig_CountsTo_mVolts(value_digit);
         Ph_mean=Ph_mean+value_ph;
-
 
     }
 
 
-    if (counter_SP==NUMBER_OF_SAMPLES){
+    if (counter_SP==NUMBER_OF_SAMPLES)
+    {
         // Format ADC result for transmition
         Ph_mean=Ph_mean/NUMBER_OF_SAMPLES; //average of 5 samples
         Temp_mean=Temp_mean/NUMBER_OF_SAMPLES;
+        
         // Write bytes in buffer
-    
         slaveBuffer[CH0_MSB] = Ph_mean >> 8;
         slaveBuffer[CH0_LSB] = Ph_mean & 0xFF;
         slaveBuffer[CH1_MSB] = Temp_mean >> 8;
--- GROUP_06.cydsn/main.c
+++ GROUP_06.cydsn/main.c
@@ -1,11 +1,6 @@
 /* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
+ * Casali Federico
+ * Sangalli Kevin
  *
  * ========================================
 */
@@ -19,35 +14,22 @@
 #define CONTROL_REG1 1
 
 
-
 uint8_t slaveBuffer[SLAVE_BUFFER_SIZE]; ///< Buffer for the slave device
-uint8_t status=0; ///control status 0)apento 1)CH temp on 2)CH ph on 3) Both on
+uint8_t status=0; ///control status 0)spento 1)CH temp on 2)CH ph on 3) Both on
 int16 period=10; //variabile per leggere il control register 1 con il periodo del timer all'interno 
-
-#define STATO0 0b00010100   //in exadecimale 14 SONO I COMANDI DA INSERIRE NEL BRIDGE CONTROL
-#define STATO1 0b00010101   //in exadecimale 15
-#define STATO2 0b00010110   //in exadecimale 16
-#define STATO3 0b00010111   //in exadecimale 17
-
-
-
 
 int main(void){
     CyGlobalIntEnable; // Enable global interrupts. 
     // Start the components
     ADC_DelSig_Start();
     AMux_Start() ;
-    
     EZI2C_Start();
     PGA_TEMP_Start();
     isr_ADC_StartEx(Custom_ISR_ADC);
-
     Timer_ADC_Start();
-    
 
     // Start the ADC conversion
     ADC_DelSig_StartConvert(); 
-
 
     // Set up Slave Buffer
     slaveBuffer[CONTROL_REG0] = 0b00010100; //primi 2 bit servono per cambiareclo stato, i bit dal 2 al 5 invece rappresentano il numero di campion usati per la media
